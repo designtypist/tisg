13d77fad670f314ed08b1d0e6c9e2dfd545fe6fc
James Chung <designtypist@gmail.com>
2020-11-26 21:22:28 -0500

    load header logo with graphql

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,36 @@
 import React from "react";
-import { Link } from 'gatsby'
+import { useStaticQuery, graphql, Link } from 'gatsby'
+import Img from "gatsby-image";
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const Header = () => (
-  <header className="flex flex-wrap md:m-12 m-6">
-    <div className="w-1/2 text-left">
-      <Link to="/">
-        <img src="tisg-logo.png" alt="TISG logo" width="50" height="50" />
-      </Link>
-    </div>
-    <div className="w-1/2 text-right">
-      <Link to="/nav/" state={{ modal: true, noScroll: true }}>
-        <FontAwesomeIcon icon={['fa', 'bars']} size="2x" className="cursor-pointer" />
-      </Link>
-    </div>
-  </header>
-);
+const Header = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      logo: file(relativePath: { eq: "icons/tisg-logo.png" }) {
+        childImageSharp {
+          fixed(width: 50, height: 50) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <header className="flex flex-wrap md:m-12 m-6">
+      <div className="w-1/2 text-left">
+        <Link to="/">
+          <Img fixed={data.logo.childImageSharp.fixed} />
+        </Link>
+      </div>
+      <div className="w-1/2 text-right">
+        <Link to="/nav/" state={{ modal: true, noScroll: true }}>
+          <FontAwesomeIcon icon={['fa', 'bars']} size="2x" className="cursor-pointer" />
+        </Link>
+      </div>
+    </header>
+  )
+}
 
 export default Header;
